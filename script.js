6ad7b08e2d908fba92669772dc12e1734afcceea
Maryam Ismail <mvart959@gmail.com>
2025-03-10 01:18:24 +0500

    changes

---
+++ script.js
@@ -0,0 +1,7 @@
+function change() {
+    let element = document.getElementById("demo");
+    element.innerText = " Its Java Script, a programming language to create actions within the page or make it dynamic or interactive";
+
+    element.style.color = "orange";
+    element.style.textAlign = "center"
+}
